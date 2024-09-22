811e088d89e6032e9df673a0161788ffdef4f0fc
PNKim <paljapon.nait@gmail.com>
2024-09-22 16:17:13 +0700

    fix: cors origin

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: '*',
+    origin: 'https://qoute-sigma.vercel.app',
     credentials: true,
   });
+
   await app.listen(4000);
 }
 bootstrap();
